57141c8ec1a21168457f63675e4e0f5e28229871
yeling117@hotmail.com <Yech@nglin1987>
2020-09-17 20:40:34 +0200

    base

--- tensquare_base/src/main/java/com/tensquare/base/pojo/Label.java
+++ tensquare_base/src/main/java/com/tensquare/base/pojo/Label.java
@@ -9,12 +9,12 @@ import java.io.Serializable;
 @Table(name = "tb_label")
 public class Label implements Serializable{
     @Id
-    private String id;//
-    private String labelname;//标签名称
-    private String state;//状态
-    private Long count;//使用数量
-    private Long fans;//关注数
-    private String recommend;//是否推荐
+    private String id;
+    private String labelname;
+    private String state;
+    private long count;
+    private long fans;
+    private String recommend;
 
     public String getId() {
         return id;
@@ -40,19 +40,19 @@ public class Label implements Serializable{
         this.state = state;
     }
 
-    public Long getCount() {
+    public long getCount() {
         return count;
     }
 
-    public void setCount(Long count) {
+    public void setCount(long count) {
         this.count = count;
     }
 
-    public Long getFans() {
+    public long getFans() {
         return fans;
     }
 
-    public void setFans(Long fans) {
+    public void setFans(long fans) {
         this.fans = fans;
     }
 
--- tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
+++ tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
@@ -32,7 +32,8 @@ public class LabelService {
         labelDao.save(label);
     }
 
-    public List<Label> findAll() {
+
+    public List<Label> findAll(String id) {
         return labelDao.findAll();
     }
 
@@ -40,61 +41,35 @@ public class LabelService {
         return labelDao.findById(id).get();
     }
 
-    public void update(Label label) {
+    /**
+     *
+     * @param label
+     */
+    public void Update(Label label) {
         labelDao.save(label);
     }
+
 
     public void delete(String labelId) {
         labelDao.deleteById(labelId);
     }
 
-    public List<Label> findSearch(Label label){
-        return labelDao.findAll(new Specification<Label>(){
+    public List<Label> findSearch(Label label) {
+        return labelDao.findAll(new Specification<Label>() {
+
+             /**
+             * @param root 根对象，也就是要把条件封装到哪个对象中。where 类名=label.getId
+             * @param criteriaQuery 封装的都是查询关键字，比如 group by , order by 等
+             * @param cb 用来封装条件对象的，如果直接返回null，表示不需要任何条件
+             * @return
+             */
+             
             @Override
-            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-                List<Predicate> list = new ArrayList<>();
-                if(label.getLabelname()!=null && !"".equals(label.getLabelname())){
-                    Predicate predicate = cb.like(root.get("labelname").as(String.class), "%" + label.getLabelname() + "%");
-                    list.add(predicate);
-                }
-                if(label.getState()!=null && !"".equals(label.getState())){
-                    Predicate predicate = cb.equal(root.get("state").as(String.class), label.getState());
-                    list.add(predicate);
-                }
-                Predicate[] parr = new Predicate[list.size()];
-                //把集合中的属性复制到数组中
-                parr = list.toArray(parr);
-                return cb.and(parr);
+            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> criteriaQuery,
+                                         CriteriaBuilder cb) {
+                return null;
             }
         });
+
     }
-
-
-    public Page<Label> pageQuery(Label label, int page, int size){
-        //封装了一个分页对象，在springdatajpa中想要实现分页，直接传一个分页对象即可
-        Pageable pageable = PageRequest.of(page-1, size);
-        return labelDao.findAll(new Specification<Label>(){
-            /*
-            * root 根对象 query封装关键字 cb封装条件对象
-            * */
-            @Override
-            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-                List<Predicate> list = new ArrayList<>();
-                if(label.getLabelname()!=null && !"".equals(label.getLabelname())){
-                    Predicate predicate = cb.like(root.get("labelname").as(String.class), "%" + label.getLabelname() + "%");
-                    list.add(predicate);
-                }
-                if(label.getState()!=null && !"".equals(label.getState())){
-                    Predicate predicate = cb.equal(root.get("state").as(String.class), label.getState());
-                    list.add(predicate);
-                }
-                Predicate[] parr = new Predicate[list.size()];
-                //把集合中的属性复制到数组中
-                parr = list.toArray(parr);
-                return cb.and(parr);
-            }
-        }, pageable);
-    }
-
-
 }
